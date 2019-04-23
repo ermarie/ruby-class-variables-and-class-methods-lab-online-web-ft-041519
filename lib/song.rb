4a202e004cbe43a085e48fbf636249e6b4ed1524
Erin Stalzer <erinstalzer@gmail.com>
2019-04-23 20:00:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  def initialized(name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @@count += 1
     @artist = artist
